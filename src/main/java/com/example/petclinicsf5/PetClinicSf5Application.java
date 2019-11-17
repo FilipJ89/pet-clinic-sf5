ab6fd4270e67f92f5ea4c0a44812c02bab9b016d
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2019-11-17 14:23:44 +0100

    Initial commit

---
+++ src/main/java/com/example/petclinicsf5/PetClinicSf5Application.java
@@ -0,0 +1,13 @@
+package com.example.petclinicsf5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSf5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSf5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinicsf5/PetClinicSf5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicsf5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSf5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
