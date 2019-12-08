ad4d67048270b384b6f306caef9830d854346734
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2019-12-08 15:25:25 +0100

    Added Vet Speciality entity and linked it with Vet class

--- src/main/java/com/example/petclinicsf5/model/VetSpeciality.java
+++ src/main/java/com/example/petclinicsf5/model/VetSpeciality.java
@@ -3,10 +3,8 @@ package com.example.petclinicsf5.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Getter
 @Setter
-public class Vet extends Person {
-    private Set<VetSpeciality> specialities;
+public class VetSpeciality extends BaseEntity {
+    private String speciality;
 }
