ddd90f548a73071580df1cd55f6cabf492b1c6ef
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2019-12-08 15:37:45 +0100

    Added PetType Map Service class and interface

---
+++ src/main/java/com/example/petclinicsf5/services/PetTypeServicw.java
@@ -0,0 +1,6 @@
+package com.example.petclinicsf5.services;
+
+import com.example.petclinicsf5.model.PetType;
+
+public interface PetTypeServicw extends CrudService<PetType, Long> {
+}
---
+++ src/main/java/com/example/petclinicsf5/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.example.petclinicsf5.services.map;
+
+import com.example.petclinicsf5.model.PetType;
+import com.example.petclinicsf5.services.PetTypeServicw;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeServicw {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+}
