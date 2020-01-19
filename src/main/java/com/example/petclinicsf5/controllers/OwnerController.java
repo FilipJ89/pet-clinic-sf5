c07e75a2ae4feb51187a2e8c2598f43d75e931b9
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2020-01-19 18:04:29 +0100

    Added HTML templates and linked ownerDetails to the project. French language messages added.

--- src/main/java/com/example/petclinicsf5/controllers/OwnerController.java
+++ src/main/java/com/example/petclinicsf5/controllers/OwnerController.java
@@ -4,6 +4,8 @@ import com.example.petclinicsf5.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
@@ -18,8 +20,14 @@ public class OwnerController {
     }
 
     @RequestMapping({"", "/", "/index", "/index.html", "/find"})
-    public String getOwnerPage(Model model) {
+    public String showAllOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("/{ownerId}")
+    public String showOwnerDetails(@PathVariable("ownerId") Long id, Model model) {
+        model.addAttribute(ownerService.findById(id));
+        return "owners/ownerDetails";
     }
 }
