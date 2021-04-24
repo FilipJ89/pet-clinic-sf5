7e5b9eee62af8f0ac723b8dfdb3657831376c377
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-04-24 18:26:36 +0200

    added option of showing vet list in JSON format

--- src/main/java/com/example/petclinicsf5/controllers/VetController.java
+++ src/main/java/com/example/petclinicsf5/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.example.petclinicsf5.controllers;
 
+import com.example.petclinicsf5.model.Vet;
 import com.example.petclinicsf5.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,10 @@ public class VetController {
     public String getVetMainPage(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping({"/api/vets", "/api/vets.html"})
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
