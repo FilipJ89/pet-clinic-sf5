80aa695c41a2abe201d5c16187f0081711c28406
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-07-18 20:10:42 +0200

    Other way of creating users & roles using AuthenticationManagerBuilder override

--- src/main/java/com/example/petclinicsf5/config/SecurityConfig.java
+++ src/main/java/com/example/petclinicsf5/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.example.petclinicsf5.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,21 +22,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().and().httpBasic();
     }
 
+    //todo -> More elegant than using bean with userDetails. {noop} means no password encoding. Other options to be tested later
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails vetUser = User.withDefaultPasswordEncoder()
-                .username("vet")
-                .password("vet")
-                .roles("VET")
-                .build();
-
-        UserDetails ownerUser = User.withDefaultPasswordEncoder()
-                .username("owner")
-                .password("owner")
-                .roles("OWNER")
-                .build();
-
-        return new InMemoryUserDetailsManager(vetUser, ownerUser);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("vet").password("{noop}vet").roles("VET")
+                .and()
+                .withUser("owner").password("{noop}owner").roles("OWNER");
     }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails vetUser = User.withDefaultPasswordEncoder()
+//                .username("vet")
+//                .password("vet")
+//                .roles("VET")
+//                .build();
+//
+//        UserDetails ownerUser = User.withDefaultPasswordEncoder()
+//                .username("owner")
+//                .password("owner")
+//                .roles("OWNER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(vetUser, ownerUser);
+//    }
 }
