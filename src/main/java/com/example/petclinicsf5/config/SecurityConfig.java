093e70b72f37f27d6af0822e15b41e03da5e911b
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-07-18 19:59:47 +0200

    Created config package with SpringSecurity class. Created 2 in-memory users and modeled free access to specific application sites

---
+++ src/main/java/com/example/petclinicsf5/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.petclinicsf5.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> {
+                    authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**", "/vets**").permitAll();
+                })
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and().httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails vetUser = User.withDefaultPasswordEncoder()
+                .username("vet")
+                .password("vet")
+                .roles("VET")
+                .build();
+
+        UserDetails ownerUser = User.withDefaultPasswordEncoder()
+                .username("owner")
+                .password("owner")
+                .roles("OWNER")
+                .build();
+
+        return new InMemoryUserDetailsManager(vetUser, ownerUser);
+    }
+}
