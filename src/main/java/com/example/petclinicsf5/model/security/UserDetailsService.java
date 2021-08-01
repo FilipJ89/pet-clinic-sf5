52ba64f9ab061240cde03060ce62d3f2935e112f
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-08-01 14:48:05 +0200

    Created UserDetailsService mapping our User to Spring Security UserDetails

---
+++ src/main/java/com/example/petclinicsf5/model/security/UserDetailsService.java
@@ -0,0 +1,44 @@
+package com.example.petclinicsf5.model.security;
+
+import com.example.petclinicsf5.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        com.example.petclinicsf5.model.security.User user = userRepository.findByUsername(username).orElseThrow(() -> {
+            return new UsernameNotFoundException("Username: '" + username + "' not found");
+        });
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),user.getPassword(),user.getEnabled(), user.getAccountNonExpired(),
+                user.getCredentialsNonExpired(),user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
+    }
+
+    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
+        if (authorities != null && authorities.size() > 0) {
+            return authorities.stream()
+                    .map(Authority::getRole)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        } else {
+            return new HashSet<>();
+        }
+    }
+}
