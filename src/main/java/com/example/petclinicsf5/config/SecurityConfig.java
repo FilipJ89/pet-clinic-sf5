de72ab06c90221a4a9f7ca28c7adcd93808c9b78
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-08-22 17:53:40 +0200

    Reconfigured messages.property. Added login form and reconfigured SpringSecurity to revert to this login template

--- src/main/java/com/example/petclinicsf5/config/SecurityConfig.java
+++ src/main/java/com/example/petclinicsf5/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().and().httpBasic();
+                .formLogin(httpSecurityFormLoginConfigurer -> {
+                    httpSecurityFormLoginConfigurer
+                            .loginProcessingUrl("/login")
+                            .loginPage("/login").permitAll()
+                            .successForwardUrl("/")
+                            .defaultSuccessUrl("/");
+                })
+                .logout(httpSecurityLogoutConfigurer -> {
+                    httpSecurityLogoutConfigurer
+                            .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                            .logoutSuccessUrl("/")
+                            .permitAll();
+                })
+                .httpBasic();
 
         // H2 console frame config
         http.headers().frameOptions().sameOrigin();
--- src/main/java/com/example/petclinicsf5/controllers/IndexController.java
+++ src/main/java/com/example/petclinicsf5/controllers/IndexController.java
@@ -1,18 +1,28 @@
 package com.example.petclinicsf5.controllers;
 
+import com.example.petclinicsf5.model.security.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @Controller
 public class IndexController {
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
 
     @GetMapping({"", "/"})
     public String showIndex(){
         return "index";
     }
 
-    @GetMapping("/oups")
-    public String showError(){
-        return "notimplemented";
+    @GetMapping("/login")
+    public String showLogin(Model model){
+        model.addAttribute("user", User.builder().build());
+        return "login";
     }
 }
