0c4585e3aaf15f46811218571f55a947fba2e087
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2021-08-29 14:42:51 +0200

    Added persistent data about users successfully logging in.

---
+++ src/main/java/com/example/petclinicsf5/config/AuthenticationEventConfig.java
@@ -0,0 +1,16 @@
+package com.example.petclinicsf5.config;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+
+@Configuration
+public class AuthenticationEventConfig {
+
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher){
+        return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    }
+}
---
+++ src/main/java/com/example/petclinicsf5/repositories/security/LoginSuccessRepository.java
@@ -0,0 +1,7 @@
+package com.example.petclinicsf5.repositories.security;
+
+import com.example.petclinicsf5.model.security.LoginSuccess;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LoginSuccessRepository extends CrudRepository<LoginSuccess, Long> {
+}
